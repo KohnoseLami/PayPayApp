26ba8a413e1ea6408dde47f467daf712a8e2d71f
KohnoseLami <64224091+KohnoseLami@users.noreply.github.com>
2022-06-03 17:05:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,19 +8,19 @@ st.markdown("#### 電話番号とパスワードを入力してください")
 
 col1, col2 = st.columns(2)
 with col1:
-    device_uuid = str(uuid.uuid4()).upper()
-    device_uuid = st.text_input("DeviceUUID", placeholder=str(uuid.uuid4()).upper(), help="デバイスのUUIDを指定できます。OTPをバイパスするために役立ちます")
+    device_uuid = st.text_input("DeviceUUID", placeholder="00000000-0000-0000-0000-000000000000", help="デバイスのUUIDを指定できます。OTPをバイパスするために役立ちます")
 with col2:
-    client_uuid = str(uuid.uuid4()).upper()
-    client_uuid = st.text_input("ClientUUID", placeholder=str(uuid.uuid4()).upper(), help="アプリのUUIDを指定できます。OTPをバイパスするために役立ちます")
+    client_uuid = st.text_input("ClientUUID", placeholder="00000000-0000-0000-0000-000000000000", help="アプリのUUIDを指定できます。OTPをバイパスするために役立ちます")
 
 phonenumber = st.text_input("電話番号")
 password = st.text_input("パスワード", type='password')
 login_event = st.button("ログイン")
 
 if phonenumber and password and login_event:
-    st.write(device_uuid)
-    st.write(client_uuid)
+    if not device_uuid:
+        device_uuid = str(uuid.uuid4()).upper()
+    if not client_uuid:
+        client_uuid = str(uuid.uuid4()).upper()
     paypay = PayPay(device_uuid=device_uuid, client_uuid=client_uuid)
     try:
         login_result = paypay.login(phonenumber, password)
