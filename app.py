7582a112f5d658d1b318c493cc4a9819d0592937
KohnoseLami <64224091+KohnoseLami@users.noreply.github.com>
2022-06-03 17:01:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ phonenumber = st.text_input("電話番号")
 password = st.text_input("パスワード", type='password')
 login_event = st.button("ログイン")
 
-if phonenumber and password and login_event and device_uuid and client_uuid:
+if phonenumber and password and login_event:
     st.write(device_uuid)
     st.write(client_uuid)
     paypay = PayPay(device_uuid=device_uuid, client_uuid=client_uuid)
